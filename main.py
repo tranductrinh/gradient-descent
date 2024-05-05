f2a8882b1eba98279bd075b5e18ecd24551d1737
Trinh Duc Tran <Trinh.Duc.Tran@mgm-tp.com>
2024-05-05 11:30:17 +0700

    Add animation

--- main.py
+++ main.py
@@ -1,25 +1,19 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib.animation import FuncAnimation
+from matplotlib.animation import PillowWriter
 
 
 def cal_y(x):
     return x ** 2 - 2 * x + 1
 
 
-plt.rcParams["figure.autolayout"] = True
-plt.title('Function y = x^2 - 2x + 1')
-plt.xlabel('x')
-plt.ylabel('y')
-x = np.linspace(-7, 7, 100)
-plt.plot(x, cal_y(x))
-
-
 def cal_derivative(x):
     return 2 * x - 2
 
 
-def gradient_descent(learning_rate, x0):
-    x = [x0]
+def gradient_descent(learning_rate, starting_point):
+    x = [starting_point]
     for it in range(1000):
         x_new = x[-1] - learning_rate * cal_derivative(x[-1])
         if abs(cal_derivative(x_new)) < 1e-6:
@@ -28,13 +22,30 @@ def gradient_descent(learning_rate, x0):
     return x, it
 
 
-(x, number_of_iterations) = gradient_descent(0.1, -5)
+(x, number_of_iterations) = gradient_descent(0.25, -5)
 print('After %d iterations, found min: x = %f, y = %f, ' % (number_of_iterations, x[-1], cal_y(x[-1])))
+print('========= Details =========')
 
 for it in x:
     print('x = %f, y = %f' % (it, cal_y(it)))
 
+fig, ax = plt.subplots(1, 1)
 
-x = np.asarray(x)
-plt.plot(x, cal_y(x), 'ro', markersize=7)
-plt.show()
+
+def animate(i):
+    ax.clear()
+    plt.title('Iteration %d: x = %f, y = %f' % (i, x[i], cal_y(x[i])))
+    plt.xlabel('x')
+    plt.ylabel('y')
+    x_org = np.linspace(-7, 9, 100)
+    plt.plot(x_org, cal_y(x_org))
+    if i > 0:
+        ax.plot(x[i - 1], cal_y(x[i - 1]), color='blue', marker='o')
+    ax.plot(x[i], cal_y(x[i]), color='red', marker='o')
+    ax.set_xlim([-7, 9])
+    ax.set_ylim([-5, 50])
+
+
+animation = FuncAnimation(fig, animate, frames=len(x), interval=500, repeat=False)
+animation.save("animation.gif", dpi=300, writer=PillowWriter(fps=1))
+plt.close()
